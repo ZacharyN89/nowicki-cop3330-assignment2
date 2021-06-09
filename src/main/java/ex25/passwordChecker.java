2ba076ad8708577bb0f2c0ef5e621e0563a79ab9
Zachary Nowicki <zacharynowicki@knights.ucf.edu>
2021-06-09 15:05:52 -0400

    ex25

---
+++ src/main/java/ex25/passwordChecker.java
@@ -0,0 +1,59 @@
+package ex25;
+
+import java.util.Arrays;
+
+public class passwordChecker {
+    public int passwordValidator(String password){
+        int passwordStrength = 0;
+
+        boolean hasNumber = false;
+        boolean hasLetter = false;
+        boolean hasSpecialCharacter = false;
+
+        char[] array = password.toCharArray();
+
+        for (char c : array) {
+            if (c >= '0' && c <= '9') {
+                hasNumber = true;
+            }
+
+            if (c >= 'a' && c <= 'z') {
+                hasLetter = true;
+            }
+
+            if (!Character.isLetterOrDigit(c)) {
+                hasSpecialCharacter = true;
+            }
+        }
+
+
+        if(array.length<8){
+            if(hasNumber && !hasLetter){
+                passwordStrength = 1;
+            }
+            if(!hasNumber && hasLetter){
+                passwordStrength = 2;
+            }
+        }else{
+            if(hasNumber && hasLetter){
+                passwordStrength = 3;
+            }
+            if(hasNumber && hasLetter && hasSpecialCharacter){
+                passwordStrength = 4;
+            }
+        }
+
+        return passwordStrength;
+    }
+
+    public String printStrength(int x, String password){
+        return switch (x) {
+            case 0 -> password + " is not a recognizable password.";
+            case 1 -> "The password '" + password + "' is a very weak password.";
+            case 2 -> "The password '" + password + "' is a weak password.";
+            case 3 -> "The password '" + password + "' is a strong password.";
+            case 4 -> "The password '" + password + "' is a very strong password.";
+            default -> "";
+        };
+    }
+}
---
+++ src/test/java/ex25/passwordCheckerTest.java
@@ -0,0 +1,64 @@
+package ex25;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class passwordCheckerTest {
+
+    @Test
+    void printStrength() {
+
+
+    }
+
+    @Test
+    void passwordValidator_just_numbers() {
+        passwordChecker test = new passwordChecker();
+        String check = "12345";
+        int result = test.passwordValidator(check);
+
+        assertEquals(1,result);
+
+    }
+
+    @Test
+    void passwordValidator_just_letter() {
+        passwordChecker test = new passwordChecker();
+        String check = "abcde";
+        int result = test.passwordValidator(check);
+
+        assertEquals(2,result);
+
+    }
+
+    @Test
+    void passwordValidator_numbers_and_letter() {
+        passwordChecker test = new passwordChecker();
+        String check = "12345ab";
+        int result = test.passwordValidator(check);
+
+        assertEquals(0,result);
+
+    }
+
+    @Test
+    void passwordValidator_numbers_letters_8() {
+        passwordChecker test = new passwordChecker();
+        String check = "12345abcde";
+        int result = test.passwordValidator(check);
+
+        assertEquals(3,result);
+
+    }
+
+    @Test
+    void passwordValidator_numbers_letters_schars_8() {
+        passwordChecker test = new passwordChecker();
+        String check = "12345abcde!";
+        int result = test.passwordValidator(check);
+
+        assertEquals(4,result);
+
+    }
+}
